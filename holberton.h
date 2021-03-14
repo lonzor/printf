5b4b258dcc640af97b28461339c81430d4a268f7
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-03-14 01:29:16 +0000

    Started header file and did some psuedocode for printf file.

--- holberton.h
+++ holberton.h
@@ -1,4 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct ops - structure for each flag for printf to process
+ *
+ *
+ *
+ **/
+struct ops
+{
+
+}
+typedef struct ops ops_t;
+
+/*function prototypes*/
+int _printf(const char *format, ...);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
 #endif
