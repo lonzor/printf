e34654f976a0a0e6f603c706b83e05098933b80f
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-03-14 01:29:16 +0000

    Started header file and did some psuedocode for printf file.

--- holberton.h
+++ holberton.h
@@ -1,4 +1,21 @@
 #ifndef _HEADER_GUARD
 #define 
 
+/**
+ * struct ops - structure for each flag for printf to process
+ *
+ *
+ *
+ **/
+struct ops
+{
+
+}
+typedef struct ops ops_t;
+
+/*function prototypes*/
+int _printf(const char *format, ...);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
 #endif
---
+++ printf.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ **/
+int _printf(const char *format, ...)
+{
+	/*some int variable to represent the num of chars
+	printed excluding the null byte*/
+
+	/*use typedef'd ops_t list array thing from header file for each flag.
+	something like 
+		{"c", char_prnt_function}
+		{"s", str_prnt_function}
+	I'm still fuzzy on how to do that, but I think we can figure it out.*/
+}
