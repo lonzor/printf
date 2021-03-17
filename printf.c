ffabfd7e360b886e1699f82ace25ffe1f8b12b40
Derek_Clemens <urwithinrange@yahoo.com>
2021-03-17 03:46:33 +0000

    Changed the layout a bit for readability

--- printf.c
+++ printf.c
@@ -1,43 +1,41 @@
 #include "holberton.h"
 /**
  * _printf - prints our desired format from the flag list
- * the we get a data type or something that doesn't work.
- * I'm not sure if we need a function that checks the input first.
- * I feel like that shoudl happen first.
+ * @format: the desired output from flags
+ * Return: count of chars that printed
  **/
 int _printf(const char *format, ...)
 {
-	int char_count= 0, j, i;
+	int char_count = 0, j, i;
 	va_list args;
-	
+
 	ops_t flags_list[] = {
 		{"c", char_func},
 		{"s", str_func},
-		/* {"%", percent_func},*/
 		{"d", int_func},
 		{"i", int_func},
+		{"r", rev_func},
 	};
-
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
 	for (i = 0; format[i] != 00; i++)
 	{
 		j = 0;
+
 		if (format[i] == '%')
-		{
-			while (j < 5 && flags_list[j].spec[0] != format[i + 1])  /* check type */
+		{ /* specifiers are compared here */
+			while (j < 5 && flags_list[j].spec[0] != format[i + 1])
 				j++;
-			if (format[i + 1] == '%')
-				i++;
 			if (j < 5) /* flag list true */
-			{
-				char_count += flags_list[j].flags(args);
+			{char_count += flags_list[j].flags(args);
 				i += 2;
 			}
-		}/*prints char if no spec is passed*/
+			if (format[i + 1] == '%') /* if %% is passed */
+				i++; /*format[i] will print % at line 42*/
+		}
 		if (format[i] != '\0')
-		{
+		{ /* printing all the chars in args */
 			_putchar(format[i]);
 			char_count++;
 		}
